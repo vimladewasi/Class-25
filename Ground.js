931979a3d7d59867b4b0b6400b230db009efd13c
Vimla Dewasi <66508837+vimladewasi@users.noreply.github.com>
2020-06-25 19:58:04 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,21 @@
+class Ground {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+
+      this.image =loadImage("sprites/ground.png");
+      }
+    display(){
+      push();
+      var pos =this.body.position;
+      imageMode(CENTER);
+      fill("brown");
+      image(this.image,pos.x, pos.y, this.width, this.height);
+      pop();
+    }
+  };
